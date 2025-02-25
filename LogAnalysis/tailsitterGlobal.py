da4c43f858073c30823b6036554586a63538efde
Till Blaha <28341894+tblaha@users.noreply.github.com>
2025-02-25 17:55:29 +0100

    finalize tailsitter model

--- LogAnalysis/tailsitterGlobal.py
+++ LogAnalysis/tailsitterGlobal.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from scipy.signal import savgol_filter
+from scipy.signal import savgol_filter, butter, sosfilt, sosfilt_zi
 
 def quaternion_rotate(q, v, inverse=False):
     # helper function to rotate a vector with a quaternion
@@ -37,20 +37,20 @@ class Tailsitter(nn.Module):
         self.r = nn.Parameter(torch.zeros(3, 1))  # IMU offset
         self.d0 = nn.Parameter(torch.zeros(2, 1)) # 0-force elevon angle
         coefficients = [
-            'cx0', 'cxw',         'cxd',  'cxv',
-            'cy0',                        'cyv',
-            'cz0', 'czw',         'czd2', 'czv',
-            'cl0', 'clw', 
-            'cm0', 'cmw',         'cmd',       'cmddd',
-            'cn0', 'cnw', 'cnwd', 'cnd'
+            'cx0', 'cxv',        'cxw',         'cxd',  'cxdd',
+            'cy0', 'cyv',
+            'cz0', 'czv',        'czw',         'czd2',
+            'cl0',        'clp', 'clw', 
+            'cm0', 'cmx', 'cmq', 'cmw',         'cmd',  'cmdd', 'cmddd',
+            'cn0',        'cnr', 'cnw', 'cnwd', 'cnd',  'cndd',
         ]
         for name in coefficients:
             setattr(self, name, nn.Parameter(torch.zeros(1)))
 
     def forward(self, x):
         # STATE
-        # body vel    body rate   body rate deriv    prop speeds (derivative)   elevon angles   elevon accelerations
-        vx, vy, vz,  Ox, Oy, Oz,   Odx, Ody, Odz,      w1, w2, w1d, w2d,           d1, d2,            d1dd, d2dd =  x
+        # body vel    body rate   body rate deriv    prop speeds (derivative)   elevon angles   elevon vel   elevon accelerations
+        vx, vy, vz,  Ox, Oy, Oz,   Odx, Ody, Odz,      w1, w2, w1d, w2d,           d1, d2,       d1d, d2d,       d1dd, d2dd =  x
         O  = torch.vstack((Ox, Oy, Oz))
         Od = torch.vstack((Odx, Ody, Odz))
 
@@ -61,14 +61,23 @@ class Tailsitter(nn.Module):
         d2abs = torch.sin( d1 ).abs() + torch.sin( d2 ).abs()         # for reduction of the prop thrust
         vxavx, vyavy, vzavz = vx.abs()*vx, vy.abs()*vy, vz.abs()*vz   # for quadratic drag
 
-        # FORCE / MOMENT MODEL
-        #     offset      prop thrust contribution         prop rate contribution           elevon contribution          elev acc contrib                  drag
-        fx = self.cx0  +  self.cxw * (ww[0] + ww[1])  +  0                        +  self.cxd * (wwDd[0] + wwDd[1])  +  0                           +  self.cxv * vxavx
-        fy = self.cy0  +  0                           +  0                        +  0                               +  0                           +  self.cyv * vyavy
-        fz = self.cz0  +  self.czw * (ww[0] + ww[1])  +  0                        +  self.czd2 * d2abs               +  0                           +  self.czv * vzavz
-        mx = self.cl0  +  self.clw * (ww[0] - ww[1])  +  0                        +  0                               +  0                           +  0
-        my = self.cm0  +  self.cmw * (ww[0] + ww[1])  +  0                        +  self.cmd * (wwDd[0] + wwDd[1])  +  self.cmddd * (d1dd + d2dd)  +  0
-        mz = self.cn0  +  self.cnw * (ww[0] - ww[1])  +  self.cnwd * (w1d - w2d)  +  self.cnd * (wwDd[0] - wwDd[1])  +  0                           +  0
+        # AERO MODEL
+        #           drag            rate damping  
+        fx  =  self.cxv * vxavx  +  0.
+        fy  =  self.cyv * vyavy  +  0.
+        fz  =  self.czv * vzavz  +  0.
+        mx  =  0.                +  self.clp * Ox
+        my  =  self.cmx * vxavx  +  self.cmq * Oy
+        mz  =  0.                +  self.cnr * Oz
+
+        # ACTUATION MODEL
+        #      prop thrust contribution         prop rate contribution           elevon contribution           elev rate contrib              elev acc contrib
+        fx +=  self.cxw * (ww[0] + ww[1])  +  0                        +  self.cxd * (wwDd[0] + wwDd[1])  +  self.cxdd * (d1d + d2d)
+        fy +=  0                           +  0                        +  0                               +  0
+        fz +=  self.czw * (ww[0] + ww[1])  +  0                        +  self.czd2 * d2abs               +  0
+        mx +=  self.clw * (ww[0] - ww[1])  +  0                        +  0                               +  0
+        my +=  self.cmw * (ww[0] + ww[1])  +  0                        +  self.cmd * (wwDd[0] + wwDd[1])  +  self.cmdd * (d1d + d2d)  +   self.cmddd * (d1dd + d2dd)
+        mz +=  self.cnw * (ww[0] - ww[1])  +  self.cnwd * (w1d - w2d)  +  self.cnd * (wwDd[0] - wwDd[1])  +  self.cndd * (d1d - d2d)
 
         f = torch.stack([fx, fy, fz])
         m = torch.stack([mx, my, mz])
@@ -78,6 +87,22 @@ class Tailsitter(nn.Module):
         dO_modelled = m # if I known: dOdt = inv(I) * (m - O.cross(self.I * O, dim=0))
 
         return torch.concat([a_modelled, dO_modelled], dim=0)
+
+model = Tailsitter()
+model.to(device=device)
+
+# ablations: keep parameters at their initial value from __init__
+#exclude = ['czd2', 'cmddd', 'cyv']
+#exclude = ['czd2', 'cyv']
+#exclude = ['czd2', 'cxw', 'cmddd']
+#exclude = ['czd2'] #, 'cmddd', 'cyv']
+#exclude = ['cxw', 'cxdd', 'cmdd', 'cndd']
+exclude = ['cxw', 'cxdd', 'cmdd', 'cndd', 'czv', 'czd2']
+#exclude = ['cxw', 'cndd']
+#exclude = []
+for par in exclude:
+    model.get_parameter(par).requires_grad = False
+
 
 class Motor(nn.Module):
     def __init__(self):
@@ -93,49 +118,52 @@ class Motor(nn.Module):
 
 
 #%% DATA loading
-log = IndiflightLog("/mnt/data/WorkData/BlackboxLogs/MIRROR_DarkO/LOG00043.BFL", resetTime=True)
 
-data = log.data[(log.data['timeS'] > 10) & (log.data['timeS'] < 50)] # todo: better flight detection
+#log = IndiflightLog("/mnt/data/WorkData/BlackboxLogs/MIRROR_DarkO/LOG00043.BFL", resetTime=True, timeRange=(10000, 50000))  # first dataset
+#log = IndiflightLog("/mnt/data/WorkData/BlackboxLogs/MIRROR_DarkO/LOG00063.BFL", resetTime=True, timeRange=(8000, 91000))   # after tuning, higher speeds
+log = IndiflightLog("/mnt/data/WorkData/BlackboxLogs/2025-02-21/LOG00066_simplifiedDynFx.BFL", resetTime=True, timeRange=(9000, 50000))   # dynamic fx, hover
+
+# preproc columns that are not in indiflight log tools yet
+data = log.data
 t = data['timeS']
 mean_dt = t.diff().mean()
+data[[f'extAtt[{i}]' for i in range(4)]] /= 8.128
+data[[f'servo_feedback[{i}]' for i in range(4)]] *= np.pi/180 / 100
+data['servo_feedback[1]'] *= 1 # set to -1 for old datafiles
 
 # convert columns to tensors
-v_true  = torch.tensor( data[[f'extVel[{i}]' for i in range(3)]].to_numpy(dtype=np.float32).T          ).to(device=device)
-q_true  = torch.tensor( data[[f'extAtt[{i}]' for i in range(4)]].to_numpy(dtype=np.float32).T / 8.128  ).to(device=device)
-#a_true  = torch.tensor( data[[f'accADCafterRpm[{i}]' for i in range(3)]].to_numpy(dtype=np.float32).T  ).to(device=device)
-a_true_raw  = torch.tensor( data[[f'accADCafterRpm[{i}]' for i in range(3)]].to_numpy(dtype=np.float32).T  ).to(device=device)
-a_true = torch.tensor( savgol_filter(a_true_raw.cpu(), window_length=9, polyorder=3) ).to(device=device)
-O_true  = torch.tensor( data[[f'gyroADCafterRpm[{i}]' for i in range(3)]].to_numpy(dtype=np.float32).T ).to(device=device)
-Od_true = torch.tensor( savgol_filter(O_true.cpu(), window_length=15, polyorder=4, deriv=1, delta=mean_dt) ).to(device=device)
-w_true_raw  = torch.tensor( data[[f'omegaUnfiltered[{i}]' for i in range(2)]].to_numpy(dtype=np.float32).T ).to(device=device)
-w_true = torch.tensor( savgol_filter(w_true_raw.cpu(), window_length=25, polyorder=4) ).to(device=device)
-wd_true = torch.tensor( savgol_filter(w_true_raw.cpu(), window_length=25, polyorder=4, deriv=1, delta=mean_dt) ).to(device=device)
-d_true_raw = torch.tensor( data[[f'servo_feedback[{i}]' for i in range(2)]] .to_numpy(dtype=np.float32).T ).to(device=device) / 100 * np.pi/180
-d_true_raw[1] *= -1
-d_true = torch.tensor( savgol_filter(d_true_raw.cpu(), window_length=50, polyorder=4) ).to(device=device)
-dd_true = torch.tensor( savgol_filter(d_true_raw.cpu(), window_length=50, polyorder=4, deriv=1, delta=mean_dt) ).to(device=device)
-ddd_true = torch.tensor( savgol_filter(d_true_raw.cpu(), window_length=50, polyorder=4, deriv=2, delta=mean_dt) ).to(device=device)
-del_12 = torch.tensor( data[[f'motor[{i}]' for i in range(2)]] .to_numpy(dtype=np.float32).T ).to(device=device)
+series = {'extVel': range(3), 'extAtt': range(4), 'accADCafterRpm': range(3),
+          'gyroADCafterRpm': range(3), 'omegaUnfiltered': range(2), 'servo_feedback': range(2),
+          'motor': range(4)}
+order = 2
+freq = 20
+butter_sos = butter(order, Wn=freq, btype='lowpass', output='sos', fs=1/mean_dt)
+for key, val in series.items():
+    tmp = data[[f'{key}[{i}]' for i in val]].to_numpy(dtype=np.float32).T
+    #tmp_f   = torch.tensor( savgol_filter(tmp, window_length=25, polyorder=3, deriv=0, delta=mean_dt) ).to(device=device)
+    zi = sosfilt_zi(butter_sos)
+    zis = np.repeat(zi, len(val), axis=0) * tmp[:, 0, np.newaxis]
+    tmp_f = torch.tensor( sosfilt(butter_sos, tmp, zi=zis[np.newaxis])[0], dtype=torch.float32 ).to(device=device)
+    tmp_fd  = torch.tensor( savgol_filter(tmp_f, window_length=25, polyorder=3, deriv=1, delta=mean_dt) ).to(device=device)
+    tmp_fdd = torch.tensor( savgol_filter(tmp_f, window_length=25, polyorder=3, deriv=2, delta=mean_dt) ).to(device=device)
+    series[key] = [tmp_f, tmp_fd, tmp_fdd]
+
+v_true = series['extVel'][0]
+q_true = series['extAtt'][0]
+a_true = series['accADCafterRpm'][0]
+O_true = series['gyroADCafterRpm'][0]
+Od_true = series['gyroADCafterRpm'][1]
+w_true = series['omegaUnfiltered'][0]
+wd_true = series['omegaUnfiltered'][1]
+d_true = series['servo_feedback'][0]
+dd_true = series['servo_feedback'][1]
+ddd_true = series['servo_feedback'][2]
+del_12 = series['motor'][0][:2]
 
 # get velocity in body frame for drag model
 v_body_true  = quaternion_rotate(q_true, v_true, inverse=True)
 
-
 #%% MODEL optimisation
-model = Tailsitter()
-model.to(device=device)
-
-# ablations: keep parameters at their initial value from __init__
-#exclude = ['cx0', 'cy0', 'cz0', 'cl0', 'cm0', 'cn0', 'czd2', 'cmddd', 'cyv']
-#exclude = ['cx0', 'cy0', 'cz0', 'cl0', 'cm0', 'cn0', 'czd2', 'cyv']
-exclude = ['cx0', 'cy0', 'cz0', 'cl0', 'cm0', 'cn0', 'czd2', 'cxw']
-#exclude = ['cx0', 'cy0', 'cz0', 'cl0', 'cm0', 'cn0', 'czd2'] #, 'cmddd', 'cyv']
-#exclude = ['cx0', 'cy0', 'cz0', 'cl0', 'cm0', 'cn0']
-#exclude = ['cx0', 'cy0', 'cz0']
-#exclude = []
-for par in exclude:
-    model.get_parameter(par).requires_grad = False
-
 # regressors and targets
 x = torch.vstack([
     v_body_true,
@@ -144,17 +172,18 @@ x = torch.vstack([
     w_true / 1000,  # to make regressors roughly unit
     wd_true / 20,
     d_true,
+    dd_true / 10,
     ddd_true / 100, # servo angular acceleration
-])[:, 1:-1]
-y_true = torch.concat((a_true, Od_true))[:, 1:-1]
+])
+y_true = torch.concat((a_true, Od_true))
 
 # loss function           accelerations   gyro derivatives
-weights = torch.tensor([[0.05, 0.05, 0.05, 0.01, 0.01, 0.01]]).T.to(device=device)
+weights = torch.tensor([[0.1, 0.1, 0.1, 0.02, 0.02, 0.02]]).T.to(device=device) / 6 / len(data)**0.5 * 10
 def weighted_mse_loss(pred, true, weight):
-    return torch.sum(weight * (pred - true) ** 2)
+    return torch.sum( (weight * (pred - true)) ** 2)
 
 optimizer = optim.LBFGS(model.parameters(), lr=5e-1); epochs = 20
-#optimizer = optim.AdamW(model.parameters(), lr=1e-1); epochs = 2000
+#optimizer = optim.AdamW(model.parameters(), lr=2e-1); epochs = 2000
 
 for epoch in range(epochs):
     def closure():
@@ -202,41 +231,47 @@ for epoch in range(epochs):
 
 
 #%% PLOTS
-fig, axs = plt.subplots(6, 2, sharex=True)
+fig, axs = plt.subplots(6, 3, sharex=True)
 fig.suptitle("DarkO Hover-model (based on motor speeds, elevon deflection, and speed)")
 fig.subplots_adjust(left=0.086, bottom=0.11, right=0.952, top=0.907)
 
 # model
 IMU_LABELS = ["a_x\ (N/kg)", "a_y\ (N/kg)", "a_z\ (N/kg)", "\dot\Omega_x\ (rad/s^2)", "\dot\Omega_y\ (rad/s^2)", "\dot\Omega_z\ (rad/s^2)"]
 for i in range(6):
-    axs[i, 0].plot(t[1:-1], y_true[i].cpu(), '-')
-    axs[i, 0].plot(t[1:-1], model.forward(x).detach().cpu()[i], '--')
+    axs[i, 0].plot(t, y_true[i].cpu(), '-')
+    axs[i, 0].plot(t, model.forward(x).detach().cpu()[i], '--')
     axs[i, 0].set_ylabel(f"${IMU_LABELS[i]}$")
 
 axs[0, 0].legend(["Raw Measured", "Modelled"])
-[ax.set_ylim((-10, 10)) for ax in axs[0:2, 0]]
+[ax.set_ylim((-15, 15)) for ax in axs[0:2, 0]]
 axs[2, 0].set_ylim((-30, 10))
 [ax.set_ylim((-150, +150)) for ax in axs[3:, 0]]
 
 # state and inputs
 VEL_LABELS = ["v_{xy}^B\ (m/s)", "v_z^B\ (m/s)"]
 for i in range(2):
-    axs[0, 1].plot(t[1:-1], v_true[i, 1:-1].cpu().T)
+    axs[0, 1].plot(t, v_body_true[i].cpu().T)
     axs[0, 1].set_ylabel(f"${VEL_LABELS[i]}$")
-    axs[0, 1].set_ylim((-3, 3))
-axs[1, 1].plot(t[1:-1], v_true[2, 1:-1].cpu().T)
+    axs[0, 1].set_ylim((-6, 6))
+axs[1, 1].plot(t, v_body_true[2].cpu().T)
 axs[1, 1].set_ylabel(f"${VEL_LABELS[1]}$")
-axs[1, 1].set_ylim((-3, 3))
+axs[1, 1].set_ylim((-4, 4))
 
 for i in range(2):
-    axs[2, 1].plot(t[1:-1], w_true[i, 1:-1].cpu().T, label=f"Motor {i+1}")
+    axs[2, 1].plot(t, w_true[i].cpu().T, label=f"Motor {i+1}")
     axs[2, 1].set_ylabel("$\omega\ (rad/s)$")
-    axs[3, 1].plot(t[1:-1], 180/np.pi*d_true[i, 1:-1].cpu().T, label=f"Elevon {i+1}")
+    axs[3, 1].plot(t, 180/np.pi*d_true[i].cpu().T, label=f"Elevon {i+1}")
     axs[3, 1].set_ylabel("$\delta\ (deg)$")
-    axs[4, 1].plot(t[1:-1], 180/np.pi*dd_true[i, 1:-1].cpu().T, label=f"Elevon {i+1}")
+    axs[4, 1].plot(t, 180/np.pi*dd_true[i].cpu().T, label=f"Elevon {i+1}")
     axs[4, 1].set_ylabel("$\dot\delta\ (deg/s)$")
-    axs[5, 1].plot(t[1:-1], 180/np.pi*ddd_true[i, 1:-1].cpu().T, label=f"Elevon {i+1}")
+    axs[5, 1].plot(t, 180/np.pi*ddd_true[i].cpu().T, label=f"Elevon {i+1}")
     axs[5, 1].set_ylabel("$\ddot\delta\ (deg/s^2)$")
+[ax.legend() for ax in axs[2:6, 1]]
+[ax.legend() for ax in axs[2:6, 1]]
+
+for i in range(3):
+    axs[0, 2].plot(t, O_true[i].cpu().T, label=f"Gyro {i+1}")
+    axs[0, 2].set_ylabel("$\Omega\ (rad/s)$")
 [ax.legend() for ax in axs[2:6, 1]]
 [ax.legend() for ax in axs[2:6, 1]]
 
@@ -254,8 +289,9 @@ Od_trim  = nn.Parameter(torch.zeros(3, 1), requires_grad=False) # body rate deri
 w_trim   = nn.Parameter(torch.ones (2, 1), requires_grad=True)  # motor speeds
 wd_trim  = nn.Parameter(torch.zeros(2, 1), requires_grad=False) # motor rate
 d_trim   = nn.Parameter(torch.zeros(2, 1), requires_grad=True)  # elevon angle
+dd_trim  = nn.Parameter(torch.zeros(2, 1), requires_grad=True)  # elevon angle first derivative
 ddd_trim = nn.Parameter(torch.zeros(2, 1), requires_grad=False) # elevon angle second derivative
-trim_pars = [v_trim, O_trim, Od_trim, w_trim, wd_trim, d_trim, ddd_trim]
+trim_pars = [v_trim, O_trim, Od_trim, w_trim, wd_trim, d_trim, dd_trim, ddd_trim]
 
 # dont allow model parameters to change, and set IMU offset to zero for the jacobians to make more sense
 from copy import deepcopy
@@ -268,8 +304,8 @@ def trim_loss(output):
     return torch.sum(output[3:, 0] ** 2)  +  ( torch.sum(output[:3, 0] ** 2) - 9.81**2 ) ** 2
 
 # optimize!
-optimizer = optim.LBFGS(trim_pars, lr=5e-1)
-for epoch in range(5):
+optimizer = optim.LBFGS(trim_pars, lr=1e-1)
+for epoch in range(100):
     def closure():
         optimizer.zero_grad()
         v = model_to_trim(torch.concat(trim_pars))
@@ -279,7 +315,7 @@ for epoch in range(5):
 
     optimizer.step(closure)
 
-    if epoch % 1 == 0:
+    if epoch % 10 == 0:
         print(f'Trimming epoch {epoch+1}: Loss = {closure().item():.5f}')
 
 
@@ -290,6 +326,7 @@ for epoch in range(5):
     #w_trim.set_(torch.tensor([[1, 1.]]).T)
     #d_trim.set_(torch.tensor([[0.8, 0.8]]).T)
 wd_trim.requires_grad = True
+dd_trim.requires_grad = True
 ddd_trim.requires_grad = True
 
 # calculate 
@@ -297,8 +334,8 @@ y = model_to_trim(torch.concat(trim_pars))
 
 # assemble jacobians 
 G1 = torch.zeros((6, 4))
-G2 = torch.zeros((6, 2))
-G3 = torch.zeros((6, 2))
+G2 = torch.zeros((6, 4))
+G3 = torch.zeros((6, 4))
 for i in range(6):
     output = torch.zeros((6, 1))
     output[i, 0] = 1.
@@ -306,7 +343,8 @@ for i in range(6):
     G1[i, 0:2] = 1/(1000**2 * 2*w_trim.T) * torch.autograd.grad(y, w_trim, grad_outputs=output, retain_graph=True)[0].T
     G1[i, 2:4] = torch.autograd.grad(y, d_trim, grad_outputs=output, retain_graph=True)[0].T
     G2[i, 0:2] = 1/20 * torch.autograd.grad(y, wd_trim, grad_outputs=output, retain_graph=True)[0].T
-    G3[i, 0:2] = 1/100 * torch.autograd.grad(y, ddd_trim, grad_outputs=output, retain_graph=True)[0].T
+    G2[i, 2:4] = 1/10 * torch.autograd.grad(y, dd_trim, grad_outputs=output, retain_graph=True)[0].T
+    G3[i, 2:4] = 1/100 * torch.autograd.grad(y, ddd_trim, grad_outputs=output, retain_graph=True)[0].T
 
 G1_indi = G1
 G2_indi = G2
@@ -315,10 +353,11 @@ G3_indi = G3
 # to u-units
 G1_indi[:, :2] *= motor.max[0] ** 2 # omega**2 = omega_max**2 * u
 G1_indi[:, 2:] *= 100*np.pi / 180   # u is in hectodegrees (...)
-G1_indi[:,  3] *= -1                # right servo is flipped
+G1_indi[:,  3] *= -1                # I have no idea why
 G1_indi[5, :2] *= -1                # I have no idea why
 G2_indi[5, :2] *= -1                # I have no idea why
-G3_indi[:,  1] *= -1                # right servo is flipped
+G2_indi[5,  2] *= 100*np.pi / 180   # u is in hectodefgrees
+G3_indi[:,  3] *= -1                # I have no idea why
 
 # to integers
 G1_indi[:3, :] *= 100
